9771d3107fed9267a67e06b2c019e153b5fa8d7c
Pery Vieira <pery.vieira@paygo-consult.com.br>
2021-10-28 19:17:34 -0300

    first commit

---
+++ src/test/java/com/jaya/currency/CurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaya.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
