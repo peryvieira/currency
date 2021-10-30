0a016eca6d56768b690ad5ad0e607ffc8be95580
Pery Vieira <peryvieira@hotmail.com>
2021-10-30 16:36:16 -0300

    fix: model's name 'User' affecting reserved word on db

--- src/main/java/com/jaya/currency/model/Transaction.java
+++ src/main/java/com/jaya/currency/model/Transaction.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -15,6 +12,7 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "transaction")
 public class Transaction {
 
     @Id
--- src/main/java/com/jaya/currency/model/Users.java
+++ src/main/java/com/jaya/currency/model/Users.java
@@ -4,16 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@Table(name = "users")
+public class Users {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
