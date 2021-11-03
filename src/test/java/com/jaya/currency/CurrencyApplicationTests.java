2efcbe4d58c306ba93e506e70420dcf35e3cca5e
Pery Vieira <peryvieira@hotmail.com>
2021-11-03 12:10:04 -0300

    test: implemented ClientServiceTests

--- src/test/java/com/jaya/currency/CurrencyApplicationTests.java
+++ src/test/java/com/jaya/currency/CurrencyApplicationTests.java
@@ -1,8 +1,11 @@
 package com.jaya.currency;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 class CurrencyApplicationTests {
 
---
+++ src/test/java/com/jaya/currency/service/impl/ClientServiceImplTest.java
@@ -0,0 +1,112 @@
+package com.jaya.currency.service.impl;
+
+import com.jaya.currency.dto.ClientDTO;
+import com.jaya.currency.exception.ClientException;
+import com.jaya.currency.model.Client;
+import com.jaya.currency.repository.ClientRepository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.inOrder;
+
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class ClientServiceImplTest {
+
+    @Mock
+    private ClientRepository clientRepository;
+
+    @InjectMocks
+    private ClientServiceImpl clientService;
+
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testCreationClientSuccess(){
+        ClientDTO clientDTO = ClientDTO.builder().name(this.clientFound().getName()).build();
+        BDDMockito.given(clientRepository.save(ClientDTO.convertToObject(clientDTO))).willReturn(this.clientFound());
+
+        clientService.save(clientDTO);
+
+        BDDMockito.then(clientRepository).should(Mockito.times(1)).save(ClientDTO.convertToObject(clientDTO));
+    }
+
+    @Test(expected = ClientException.class)
+    public void testCreationClientException(){
+        ClientDTO clientDTO = ClientDTO.builder().name(this.clientFound().getName()).build();
+        BDDMockito.given(clientRepository.save(ClientDTO.convertToObject(clientDTO))).willThrow(new ClientException("Client cannot be saved on database", HttpStatus.INTERNAL_SERVER_ERROR));
+
+        clientService.save(clientDTO);
+    }
+
+    @Test
+    public void testFindClientByIdSuccess(){
+        BDDMockito.given(clientRepository.findById(1L)).willReturn(Optional.of(this.clientFound()));
+
+        clientService.findById(1L);
+
+        BDDMockito.then(clientRepository).should(Mockito.times(1)).findById(1L);
+    }
+
+    @Test
+    public void testFindAllClientsSuccess(){
+        BDDMockito.given(clientRepository.findAll()).willReturn(this.clientListFound());
+
+        clientService.findAll();
+
+        BDDMockito.then(clientRepository).should(Mockito.times(1)).findAll();
+    }
+
+    @Test
+    public void testDeleteClientSuccess(){
+        BDDMockito.given(clientRepository.findById(1L)).willReturn(Optional.of(this.clientFound()));
+        BDDMockito.willDoNothing().given(clientRepository).delete(this.clientFound());
+
+        clientService.delete(1L);
+
+        InOrder inOrder = inOrder(clientRepository);
+        inOrder.verify(clientRepository, Mockito.times(1)).findById(1L);
+        inOrder.verify(clientRepository, Mockito.times(1)).delete(this.clientFound());
+    }
+
+
+    private Client clientFound(){
+        return Client.builder()
+                .id(1L)
+                .name("Pery")
+                .createdAt(LocalDate.now()).build();
+    }
+
+    private List<Client> clientListFound(){
+        List<Client> list = new ArrayList<>();
+
+        list.add(Client.builder()
+                .id(1L)
+                .name("Pery")
+                .createdAt(LocalDate.now()).build());
+
+        list.add(Client.builder()
+                .id(2L)
+                .name("Paulo")
+                .createdAt(LocalDate.now()).build());
+
+        return list;
+    }
+
+
+}
