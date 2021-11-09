68d413f0a15d99b72e0075722d56b8bc67f4efcd
Pery Vieira <peryvieira@hotmail.com>
2021-11-08 23:17:09 -0300

    test: API test on ClientCOntroller

--- src/test/java/com/jaya/currency/api/ClientApiTest.java
+++ src/test/java/com/jaya/currency/api/ClientApiTest.java
@@ -2,21 +2,53 @@ package com.jaya.currency.api;
 
 import io.restassured.http.ContentType;
 import org.json.JSONException;
-import org.junit.Ignore;
+import org.json.JSONObject;
 import org.junit.Test;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.containsString;
 
 
 public class ClientApiTest extends BaseApi{
 
-
     @Test
-    public void saveClient() throws JSONException {
+    public void saveClientSuccess() throws JSONException {
 
-        given().body("{\n" +
-                "   \"name\": \"Pery\"}")
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("name","Pery");
+
+        given().body(requestParams.toString())
                 .contentType(ContentType.JSON)
                 .when().post("/v1/client")
-                .then().assertThat().statusCode(201);
+                .then().assertThat().statusCode(201).body(containsString("id")).extract().response();
+
+    }
+
+    @Test
+    public void saveClientException() throws JSONException{
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("nome","Pery");
+
+        given().body(requestParams.toString())
+                .contentType(ContentType.JSON)
+                .when().post("/v1/client")
+                .then().assertThat().statusCode(400).body(containsString("Client name cannot be null"));
+    }
+
+    @Test
+    public void findClientSuccess() {
+        Long idClient = 1L;
+
+        given().when().get("/v1/client/"+idClient)
+                .then().assertThat().statusCode(200)
+                .body(containsString("id"),containsString("name"), containsString("createdAt"));
+    }
+
+    @Test
+    public void findAllClientsSuccess(){
+        given().when().get("/v1/client")
+                .then().assertThat().statusCode(200)
+                .body(containsString("id"),containsString("name"), containsString("createdAt"));
     }
 }
+
